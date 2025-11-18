31b95bc77087d6561aa2573cd3888bb76a300099
IsratErin <israterin11@gmail.com>
2025-11-18 12:52:56 +0100

    verify firebase token in the backend middleware

---
+++ backend/middleware/authMiddleware.ts
@@ -0,0 +1,32 @@
+import admin from "../firebase/firebaseAdmin";
+import { Request, Response, NextFunction } from "express";
+
+interface AuthenticatedRequest extends Request {
+  user?: admin.auth.DecodedIdToken;
+}
+
+const verifyIdToken = async (
+  req: AuthenticatedRequest,
+  res: Response,
+  next: NextFunction
+) => {
+  const authHeader = req.headers.authorization;
+
+  if (!authHeader || !authHeader.startsWith("Bearer ")) {
+    console.error("No token provided");
+    return res.status(401).json({ message: "Unauthorized" });
+  }
+
+  const token = authHeader.split(" ")[1];
+
+  try {
+    const decodedToken = await admin.auth().verifyIdToken(token);
+    req.user = decodedToken;
+    next();
+  } catch (error) {
+    console.error("Error verifying token:", error);
+    res.status(401).json({ message: "Unauthorized" });
+  }
+};
+
+export default verifyIdToken;
